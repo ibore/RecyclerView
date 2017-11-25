13fafe32e4e382a366b22bd034ba8a05165830b6
Ibore Xie <bore521@live.com>
2017-11-26 00:33:49 +0800

    Initial commit

---
+++ recycler/src/main/java/me/ibore/widget/recycler/AbsAdapter.java
@@ -0,0 +1,28 @@
+package me.ibore.widget.recycler;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * description:
+ * author: Ibore Xie
+ * date: 2017-11-26 00:24
+ * website: ibore.me
+ */
+
+public class AbsAdapter extends RecyclerView.Adapter<ViewHolder> {
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
