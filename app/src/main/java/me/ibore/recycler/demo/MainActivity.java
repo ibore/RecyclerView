054094114c05de8cc2c8b24886a43343466635ae
Ibore Xie <bore521@live.com>
2017-12-01 00:06:57 +0800

    1201

--- app/src/main/java/me/ibore/recycler/demo/MainActivity.java
+++ app/src/main/java/me/ibore/recycler/demo/MainActivity.java
@@ -1,7 +1,13 @@
 package me.ibore.recycler.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import me.ibore.widget.recycler.RecyclerAdapter;
+import me.ibore.widget.recycler.RecyclerHolder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerAdapter<String, RecyclerHolder> adapter = new RecyclerAdapter<String, RecyclerHolder>() {
+            @Override
+            protected RecyclerHolder onCreateRecyclerViewHolder(ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            protected void onBindRecyclerViewHolder(RecyclerHolder holder, List mDatas, int position) {
+
+            }
+        };
     }
 }
